cfc4d1d06b9b750291519a4c94971c46d3f241f3
grumpycoder <grumpycoder@outlook.com>
2014-09-03 16:07:47 -0500

    Added Person model

---
+++ WOT.Server/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WOT.Server.Models
+{
+    public class Person
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [StringLength(100)]
+        public string Name { get; set; }
+        public bool? IsVIP { get; set; }
+        public int? Priority { get; set; }
+    }
+}
