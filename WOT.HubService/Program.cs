de102a11a5e2a7811f18fd334e11f4e4d15d465c
grumpycoder <grumpycoder@outlook.com>
2014-09-24 13:28:36 -0500

    Code refactor WOT.Hub

--- WOT.HubService/Program.cs
+++ WOT.HubService/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Threading.Tasks;
-using Microsoft.AspNet.SignalR;
-using Microsoft.Owin.Cors;
 using Microsoft.Owin.Hosting;
-using Owin;
 using WOT.HubService.Properties;
 
 namespace WOT.HubService
@@ -23,35 +19,5 @@ namespace WOT.HubService
             }
         }
      
-    }
-
-    class Startup
-    {
-        public void Configuration(IAppBuilder app)
-        {
-            app.UseCors(CorsOptions.AllowAll);
-            app.MapSignalR();
-        }
-    }
-
-    public class WallHub : Hub
-    {
-        public void Send(string kiosk, string message)
-        {
-            Clients.All.sendName(kiosk, message);
-            Console.WriteLine("{0} sent {1}", kiosk, message);
-        }
-
-        public override Task OnConnected()
-        {
-            Console.WriteLine("{0} Connected", Context.ConnectionId);
-            return base.OnConnected();
-        }
-
-        public override Task OnDisconnected(bool stopCalled)
-        {
-            Console.WriteLine("{0} Disconnected", Context.ConnectionId);
-            return base.OnDisconnected(stopCalled);
-        }
     }
 }
---
+++ WOT.HubService/WallHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace WOT.HubService
+{
+    public class WallHub : Hub
+    {
+        public void Send(string kiosk, string message)
+        {
+            Clients.All.sendName(kiosk, message);
+            Console.WriteLine("{0} sent {1}", kiosk, message);
+        }
+
+        public override Task OnConnected()
+        {
+            Console.WriteLine("{0} Connected", Context.ConnectionId);
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            Console.WriteLine("{0} Disconnected", Context.ConnectionId);
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
